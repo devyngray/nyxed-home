d46fe4ad1c5ee87a735ba2bc835a8b3bf7cb48f9
Devyn Gray <devyngray@proton.me>
2026-03-23 14:30:03 -0400

    nushell better config

--- dev/shell.nix
+++ dev/shell.nix
@@ -10,7 +10,40 @@ in
 {
   config = lib.mkIf cfg.enable {
     programs.bash.enable = true;
-    programs.nushell.enable = true;
+
+    programs.nushell = {
+      enable = true;
+      extraConfig = ''
+        let carapace_completer = {|spans|
+        carapace $spans.0 nushell ...$spans | from json
+        }
+        $env.config = {
+         table: {
+           mode: "compact",
+         },
+         show_banner: false,
+         completions: {
+         case_sensitive: false # case-sensitive completions
+         quick: true    # set to false to prevent auto-selecting completions
+         partial: true    # set to false to prevent partial filling of the prompt
+         algorithm: "fuzzy"    # prefix or fuzzy
+         external: {
+         # set to false to prevent nushell looking into $env.PATH to find more suggestions
+             enable: true 
+         # set to lower can improve completion performance at the cost of omitting some options
+             max_results: 100 
+             completer: $carapace_completer # check 'carapace_completer' 
+           }
+         }
+        } 
+      '';
+    };
+
+    programs.carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+    };
+
     home.sessionVariables = {
       PAGER = "less -FR";
     };
